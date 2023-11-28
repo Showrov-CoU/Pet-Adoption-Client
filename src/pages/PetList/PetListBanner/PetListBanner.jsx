e9ce9ac7b47d74f7a1a2d9ce853fcc652da05933
Md.Ashikur Rahman (Showrov) <showrov.cse.cou@gmail.com>
2023-11-29 03:15:35 +0600

    change petlist dropdown

--- src/pages/PetList/PetListBanner/PetListBanner.jsx
+++ src/pages/PetList/PetListBanner/PetListBanner.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import Pets from "../Pets/Pets";
 import axios from "axios";
+import { Dropdown } from "flowbite-react";
 // import usePets from "../../../Hooks/usePets";
 // import Pets from "../Pets/Pets";
 
@@ -34,53 +35,19 @@ const PetListBanner = () => {
         </p>
 
         <div className=" mx-auto text-center md:max-w-screen-md lg:max-w-screen-lg lg:px-36">
-          {/* dropdown */}
           <div className="flex gap-20 flex-wrap justify-center items-center mt-8 text-gray-500 sm:justify-between">
-            <button
-              id="dropdownHoverButton"
-              data-dropdown-toggle="dropdownHover"
-              data-dropdown-trigger="hover"
-              className="text-white bg-primary hover:bg-primaryDeep focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center"
-              type="button"
+            {/* dropdown */}
+            <Dropdown
+              className="text-white font-bold"
+              gradientMonochrome="lime"
+              label="Select menu for seeing pets"
             >
-              Select menu for seeing pets{" "}
-              <svg
-                className="w-2.5 h-2.5 ms-3"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 10 6"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="m1 1 4 4 4-4"
-                />
-              </svg>
-            </button>
-
-            <div
-              id="dropdownHover"
-              className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"
-            >
-              <ul
-                className="py-2 text-sm text-gray-700 dark:text-gray-200"
-                aria-labelledby="dropdownHoverButton"
-              >
-                {petsArray.map((pet) => (
-                  <li key={pet}>
-                    <button
-                      onClick={() => handlePet(pet)}
-                      className="w-full text-start px-4 py-2 hover:bg-primary hover:text-white"
-                    >
-                      {pet}
-                    </button>
-                  </li>
-                ))}
-              </ul>
-            </div>
+              {petsArray.map((pet) => (
+                <Dropdown.Item onClick={() => handlePet(pet)} key={pet}>
+                  {pet}
+                </Dropdown.Item>
+              ))}
+            </Dropdown>
 
             {/* search */}
 
